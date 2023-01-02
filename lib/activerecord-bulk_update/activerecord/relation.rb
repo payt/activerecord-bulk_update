7edcf148e823d43a0dedfc2ae03286cc5a405180
Nicke van Oorschot <nickevanoorschot@gmail.com>
2023-01-02 10:41:51 +0100

    BulkInvalid

--- lib/activerecord-bulk_update/activerecord/relation.rb
+++ lib/activerecord-bulk_update/activerecord/relation.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 module ActiveRecord
+  class BulkInvalid < ActiveRecordError; end
+
   class Relation
     def bulk_create(*args, **kwargs)
       bulk_create!(*args, **kwargs)
-    rescue ActiveRecord::RecordInvalid
+    rescue ActiveRecord::BulkInvalid
       false
     end
 
     def bulk_create!(inserts, ignore_persisted: false, touch: true, validate: true)
-      raise ActiveRecord::RecordInvalid if validate && inserts.map(&:valid?).any?(false)
+      raise ActiveRecord::BulkInvalid, bulk_errors(inserts) if validate && bulk_invalid?(inserts)
 
       bulk_insert(inserts, ignore_persisted: ignore_persisted, touch: touch)
 
@@ -30,12 +32,12 @@ module ActiveRecord
 
     def bulk_update(*args, **kwargs)
       bulk_update!(*args, **kwargs)
-    rescue ActiveRecord::RecordInvalid
+    rescue ActiveRecord::BulkInvalid
       false
     end
 
     def bulk_update!(updates, touch: true, validate: true)
-      raise ActiveRecord::RecordInvalid if validate && updates.map(&:valid?).any?(false)
+      raise ActiveRecord::BulkInvalid, bulk_errors(updates) if validate && bulk_invalid?(updates)
 
       BulkUpdate.new(self, updates, touch: touch).update_records
 
@@ -48,6 +50,22 @@ module ActiveRecord
 
     def bulk_update_columns(updates, touch: false)
       BulkUpdate.new(self, updates, touch: touch).update_records
+    end
+
+    def bulk_valid?(records)
+      records.map(&:valid?).all?(true)
+    end
+
+    def bulk_invalid?(records)
+      !bulk_valid?(records)
+    end
+
+    def bulk_errors(records)
+      records.map.with_index do |record, index|
+        next if record.errors.none?
+
+        { index: index, id: record.id_in_database, errors: record.errors.details }
+      end.compact
     end
   end
 end
