cf7a723c247df8782ebbe8c6d74a170956e9a1f6
Bob van Oorschot <bobvanoorschot@gmail.com>
2023-04-12 19:28:41 +0200

    Test if records are updated correctly

--- test/activerecord/bulk_upsert_test.rb
+++ test/activerecord/bulk_upsert_test.rb
@@ -14,7 +14,7 @@ module ActiveRecord
 
       before do
         @model = FakeRecord.all
-        @upserts = [FakeRecord.new(name: "1ste", rank: 1), FakeRecord.new(name: "2nd")]
+        @upserts = [FakeRecord.new(name: "1ste", rank: 1, active: false), FakeRecord.new(name: "2nd")]
         @touch = false
         @ignore_persisted = false
         @unique_key = nil
@@ -26,23 +26,24 @@ module ActiveRecord
 
       describe "when unique_by is given" do
         before do
-          FakeRecord.create(name: "1ste", rank: 1)
+          FakeRecord.create(name: "1ste", rank: 1, active: true)
           @unique_key = [:name, :rank]
         end
 
         it "Updates one and creates one" do
-          assert_change(-> { FakeRecord.count }, by: 1) { upsert_records }
-        end
-
-        it "Updates one and creates one" do
-          assert_change(-> { FakeRecord.first.updated_at }) do
-            Kernel.sleep 0.01
-            upsert_records
+            assert_change(-> { FakeRecord.count }, by: 1) { upsert_records }
           end
+
+        it "update the active state of the duplicate" do
+          assert_change(-> { FakeRecord.where(name: "1ste", rank: 1).take.active }, from: true, to: false) { upsert_records }
         end
 
-        it "Do not change created_at" do
-          refute_change(-> { FakeRecord.first.created_at }) { upsert_records }
+        it "updates the updated_at timestamp" do
+          assert_change(-> { FakeRecord.where(name: "1ste", rank: 1).take.updated_at }) { upsert_records }
+        end
+
+        it "does not update the created_at" do
+          refute_change(-> { FakeRecord.where(name: "1ste", rank: 1).take.created_at }) { upsert_records }
         end
       end
 
