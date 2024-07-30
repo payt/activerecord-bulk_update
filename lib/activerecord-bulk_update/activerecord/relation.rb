8e258f5e8e9c9de767b53e1fb8c6ac04bc3c5552
Laura Baakman <l.baakman@paytsoftware.com>
2024-07-30 12:04:31 +0200

    add rdoc to `bulk_insert` and `bulk_insert!`

--- lib/activerecord-bulk_update/activerecord/relation.rb
+++ lib/activerecord-bulk_update/activerecord/relation.rb
@@ -26,10 +26,33 @@ module ActiveRecord
       BulkDelete.new(self, deletes).delete_by_filters
     end
 
+    # Inserts multiple records into the database in a single query.
+    #
+    # @example
+    #
+    # users = [User.new(name: "foo"), User.new(name: "bar")]
+    # User.where(active: true).bulk_insert(users)
+    #
+    # @param [Record] inserts The records to insert.
+    # @param [Object] ignore_persisted If truthy any persisted records are ignored.
+    # @param [Object] ignore_duplicates If truthy any duplicate records are ignored.
+    # @param [Object] unique_by Uniqueness constraint to skip rows by. Requires +ignore_duplicates+ to be
+    #   falsey. {ActiveRecord::Persistence::ClassMethods.insert_all See ActiveRecord's insert_all}
+    #
+    # @see {ActiveRecord::Persistence::ClassMethods.insert_all ActiveRecord's insert_all}
+    #
+    # @raise [ActiveRecord::ActiveRecordError] if inserts contains persisted records and ignore_persisted is false.
+    # @raise [ActiveRecord::RecordNotUnique] if inserts contains duplicated records and ignore_duplicates is false.
+    # @raise [ArgumentError] if both +unique_by+ and +ignore_duplicates+ are provided.
+    #
+    # @return [ActiveRecord::Result] containing the records as they have been inserted.
     def bulk_insert(inserts, ignore_persisted: false, ignore_duplicates: true, unique_by: nil)
       BulkInsert.new(self, inserts, ignore_persisted: ignore_persisted, ignore_duplicates: ignore_duplicates, unique_by: unique_by).insert_records
     end
 
+    # Inserts multiple records into the database in a single query.
+    #
+    # @see {.bulk_insert}
     def bulk_insert!(inserts, ignore_persisted: false, ignore_duplicates: false, unique_by: nil)
       BulkInsert.new(self, inserts, ignore_persisted: ignore_persisted, ignore_duplicates: ignore_duplicates, unique_by: unique_by).insert_records
     end
